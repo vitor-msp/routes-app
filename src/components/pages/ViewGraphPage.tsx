0d61a57ad9d4f26df88b009b5388c0268f05810e
Vitor Mateus <vitor7jan@gmail.com>
2022-05-14 21:07:44 -0300

    set bootstrap css

--- src/components/pages/ViewGraphPage.tsx
+++ src/components/pages/ViewGraphPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { getGraphRequest } from "../../store/ducks/graph/graph.middleware";
