b20c5bbb247f7081d267bd997bfd7d75c0c88fb8
Vitor Mateus <vitor7jan@gmail.com>
2024-10-26 11:38:55 -0300

    added api env var

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import { GetMinRouteDTO } from "../store/ducks/path/path.types";
 import { GetRoutesDTO } from "../store/ducks/routes/routes.types";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-type": "application/json",
   },
